0f5e6bc32f6a40b0d5fc99eadcd43f67ed48fba0
AnaikaTyagi <87835570+AnaikaTyagi@users.noreply.github.com>
2024-07-31 18:18:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,11 @@ anthropic_api_key = st.secrets["token"]
 st.title("Learn More About Your Source")
 st.write(
     "Upload a document below! We will extract key points and provide a summary. You can also ask a question about the source for clarification or additional information."
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 uploaded_file = st.file_uploader("Upload a source", type=("txt","md"))
 extracted_data = "extract key data points from this source"
 summary = "give me a brief summary of this source"
-queston = st.text_input(
+question = st.text_input(
     "Ask something about the source",
     disables=not uploaded_file,
 )
