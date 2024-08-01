a6cf15961c9c0359b6aa6349ac4175ac1f41b497
AnaikaTyagi <87835570+AnaikaTyagi@users.noreply.github.com>
2024-08-01 13:38:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ question = st.text_input(
 )
 
 if uploaded_file:
-    
+    article = uploaded_file.read().decode()
     prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
     {article}\n\n</article>\n\n{extracted_data}{anthropic.AI_PROMPT}"""
 
@@ -27,7 +27,7 @@ if uploaded_file:
         prompt=prompt,
         stop_sequences=[anthropic.HUMAN_PROMPT],
         model="claude-v3.5",
-        max_tokens_to_sample=2,
+        max_tokens_to_sample=100,
     )
     st.write("### Answer")
     st.write(response.completion)
